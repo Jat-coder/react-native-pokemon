9392b55b6797439825a9661ea351e396dbb79a0a
Bhaskar <jatprogrammer@gmail.com>
2021-12-09 21:59:41 +0530

    Api request Made

--- SearchScreen/searchScreen.js
+++ SearchScreen/searchScreen.js
@@ -1,14 +1,28 @@
 import SearchBar from "../Utils/SearchBar";
 import {View,Text,StyleSheet} from 'react-native';
 import React, {useState} from 'react';
+import yelp from '../Utils/apiYup'
 const SearchScreen = () =>{
+    const searchApi = async()=>{
+        const response = await yelp.get('/applin')
+        console.log(response);
+        setResults(response);
+    }
     const [term,setTerm] = useState('');
+    const [results,setResults] = useState([]);
     return(
         <View>
-            <SearchBar term={term} onTermChange={newTerm=>setTerm(newTerm)}/>
-            <Text>{term}</Text>
+            <SearchBar onTermSubmit={()=>{console.log('s8ubmitted');searchApi();}} term={term} onTermChange={newTerm=>setTerm(newTerm)}/>
+            <View style={styles.screen}>
+            <Text>found {results.length} results</Text>
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    screen:{
+        margin:15,
+    }
+})
 
 export default SearchScreen;
